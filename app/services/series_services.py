3d9d70b17a086d8604eb7d94d3d38e972165e69b
Humberto <matheushumberto520@gmail.com>
2021-09-17 07:52:34 -0400

    Final project

--- app/services/series_services.py
+++ app/services/series_services.py
@@ -1,11 +1,11 @@
 import psycopg2
 from psycopg2 import sql
 from app.services.configs import configs
+from app.exc.exc import IdNotFound
 
 
 def create_table():
     conn = psycopg2.connect(**configs)
-
     cur = conn.cursor()
 
     query = sql.SQL(
@@ -14,15 +14,114 @@ def create_table():
             id BIGSERIAL PRIMARY KEY,
             serie VARCHAR(100) NOT NULL UNIQUE,
             seasons INTEGER NOT NULL,
-            released_data DATE NOT NULL,
+            released_date DATE NOT NULL,
             genre VARCHAR(50) NOT NULL,
-            imbd_rating FLOAT NOT NULl
+            imdb_rating FLOAT NOT NULl
         );
         """
     )
     cur.execute(query)
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 class Series():
-    def __init__(self) -> None:
-        pass
+    def __init__(
+            self, serie, seasons, released_date, genre, imdb_rating
+            ) -> None:
+        self.serie = serie.title()
+        self.seasons = seasons
+        self.released_date = released_date
+        self.genre = genre.title()
+        self.imdb_rating = imdb_rating
+
+    def save(self):
+        create_table()
+
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        keys = [
+            "id", "serie", "seasons", "released_date", "genre", "imdb_rating"
+        ]
+        columns = [sql.Identifier(key) for key in self.__dict__.keys()]
+        values = [sql.Literal(value) for value in self.__dict__.values()]
+
+        query = sql.SQL(
+            """
+                INSERT INTO
+                    ka_series (id, {columns})
+                VALUES
+                    (DEFAULT, {values})
+                RETURNING *;
+            """
+        ).format(
+            columns=sql.SQL(',').join(columns),
+            values=sql.SQL(',').join(values)
+        )
+
+        cur.execute(query)
+        fetch_result = cur.fetchone()
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        serialized_data = dict(zip(keys, fetch_result))
+
+        return serialized_data
+
+    def get_all():
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        columns = [
+            "id", "serie", "seasons", "released_date", "genre", "imdb_rating"
+        ]
+
+        cur.execute(""" SELECT * FROM ka_series; """)
+
+        fetch_result = cur.fetchall()
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        serialized_data = [
+            dict(zip(columns, serie_data)) for serie_data in fetch_result
+        ]
+
+        return {"data": serialized_data}
+
+    def get_by_id(series_id: int):
+
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        columns = [
+            "id", "serie", "seasons", "released_date", "genre", "imdb_rating"
+        ]
+
+        cur.execute(
+            """
+                SELECT
+                    *
+                FROM
+                    ka_series
+                WHERE
+                    id=(%s);
+            """, (series_id, ))
+
+        fetch_result = cur.fetchone()
+
+        if not fetch_result:
+            raise IdNotFound("Not found.")
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        serialized_data = {"data": dict(zip(columns, fetch_result))}
+
+        return serialized_data
--- app/views/series_views.py
+++ app/views/series_views.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from app.exc.exc import IdNotFound
+from app.services.series_services import Series
+from flask import Flask, request, jsonify
+from psycopg2.errors import UniqueViolation, UndefinedTable
 
 
 def init_app(app: Flask):
     @app.post("/series")
     def create():
-        ...
+        try:
+            data = request.json
+            serie = Series(**data)
+            saved = serie.save()
+            return saved, 201
+        except UniqueViolation as err:
+            return {"message": str(err).split("\n")[-2]}, 409
+
+        except TypeError as err:
+            return {"message": str(err).split("an ")[-1]}, 400
 
     @app.get("/series")
     def series():
-        ...
+        try:
+            series_list = Series.get_all()
+            return jsonify(series_list), 200
+
+        except UndefinedTable:
+            return {"data": []}, 200
 
     @app.get("/series/<int:series_id>")
-    def select_by_id():
-        ...
+    def select_by_id(series_id: int):
+        try:
+            serie = Series.get_by_id(series_id)
+            return serie, 200
+
+        except IdNotFound as err:
+            return {"error": str(err)}, 404
+
+        except UndefinedTable:
+            return jsonify([]), 200
